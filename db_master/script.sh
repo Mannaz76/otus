6f130455b10f9e39aecfdf3ae71f949c4d0b20ff
mannaz <artem.bezzubtsev@yandex.ru>
2025-09-30 13:18:22 +0000

    add bd

--- db_master/script.sh
+++ db_master/script.sh
@@ -10,10 +10,13 @@ cp ./otus/db_master/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 
 systemctl restart mysql
 
-mysql -u root << EOF
+mysql << EOF
 
 CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'oTUSlave#2020';
 GRANT REPLICATION SLAVE ON *.* TO repl@'%';
+CREATE DATABASE sakila;
 EOF
+
+mysql sakila < ./otus/db_master/sakila-data.sql
 
 
