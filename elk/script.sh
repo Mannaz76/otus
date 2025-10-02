44409b237f9577ccd3469b3670d514dd5db7e6c3
mannaz <artem.bezzubtsev@yandex.ru>
2025-10-02 06:45:16 +0000

    add elk

---
+++ elk/script.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+password=$1
+
+#обновляем репо и уcтанавливаем mysql
+apt update && apt install default-jbk nginx -y
+
+
+dpkg -i ./otus/elk/*.deb
+
+# Задаем лимиты памяти вля виртуальной машины Java
+cp ./otus/elk/jvm.options /etc/elasticsearch/jvm.options.d/
+
+#
+cp ./otus/elk/elasticsearch.yml /etc/elasticsearch/
+
+#запускаем
+systemctl daemon-reload
+systemctl enable --now elasticsearch.service
+
+
+
+/usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic -i << EOF
+Y
+$password
+$password
+EOF
+
+systemctl daemon-reload
+systemctl enable --now kibana.service
+
+cp ./otus/elk/kibana.yml /etc/kibana/
+
+# Проверка конфигурации
+/usr/share/kibana/bin/kibana test config
+
+systemctl restart kibana
+
+# Генеририруем токен
+/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana > elasticsearch-token
+
+# Код верификации
+/usr/share/kibana/bin/kibana-verification-code > kibana-verification-code
+
+# Настройка Logstash
+systemctl enable --now logstash.service
+
+# Копируем корневой сертификат от elasticsearch
+mkdir /etc/logstash/certs/
+cp /etc/elasticsearch/certs/http_ca.crt /etc/logstash/certs/
+chown -R logstash:logstash /etc/logstash/certs/
+
+cp ./otus/elk/logstash.yml /etc/logstash/
+
+
+cat > /etc/logstash/conf.d/logstash-nginx-es.conf << EOF
+
+input {
+    beats {
+        port => 5400
+    }
+}
+
+filter {
+ grok {
+   match => [ "message" , "%{COMBINEDAPACHELOG}+%{GREEDYDATA:extra_fields}"]
+   overwrite => [ "message" ]
+ }
+ mutate {
+   convert => ["response", "integer"]
+   convert => ["bytes", "integer"]
+   convert => ["responsetime", "float"]
+ }
+ date {
+   match => [ "timestamp" , "dd/MMM/YYYY:HH:mm:ss Z" ]
+   remove_field => [ "timestamp" ]
+ }
+}
+
+output {
+ elasticsearch {
+   hosts => ["https://localhost:9200"]
+   user => "elastic"
+   password => "$password"
+   index => "weblogs-%{+YYYY.MM.dd}"
+   document_type => "nginx_logs"
+   cacert => "/etc/logstash/certs/http_ca.crt"
+ }
+ stdout { codec => rubydebug }
+}
+EOF
+
+
+systemctl restart logstash.service
+
+
+
+
+
+
+
