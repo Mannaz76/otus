8036487a0b2594ec4b36fad1dc6b94f49791864f
Mannaz76 <artem.bezzubtsev@yandex.ru>
2025-10-17 10:58:10 +0300

    Update script.sh

--- frontend/script.sh
+++ frontend/script.sh
@@ -6,7 +6,7 @@ cp ./otus/frontend/default /etc/nginx/sites-available/
 
 nginx -t && systemctl reload nginx
 
-dpkg -i ./otus/frontend/*.deb
+dpkg -i ./nas/elk/filebeat_8.17.1_amd64.deb
 
 cp ./otus/frontend/filebeat.yml /etc/filebeat/
 
